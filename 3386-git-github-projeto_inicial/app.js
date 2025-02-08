af8acc3a7b0fcf0d95432c6ec9f7c9da2a0b0b69
gilsondultra <gdultra@uneb.br>
2025-02-08 07:53:11 -0300

    Primeiro commit - number

---
+++ 3386-git-github-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+let numeroInput = prompt('Digite um número: ');
+
+if (numeroInput > 0) {
+    alert('Este número é positivo.');
+} else if (numeroInput < 0) {    
+    alert('Este número é negativo!');
+} else {
+    alert('Zero é neutro.')
+}
+
+
