ea8685a3f9fc39f9c516aef245c51aa3a576120d
gilsondultra <gdultra@uneb.br>
2025-02-10 20:43:46 -0300

    add gitignore

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -1,4 +1,4 @@
-let numeroInput = prompt('Digite um número: ');
+/*let numeroInput = prompt('Digite um número: ');
 
 if (numeroInput > 0) {
     alert('Este número é positivo.');
@@ -6,6 +6,26 @@ if (numeroInput > 0) {
     alert('Este número é negativo!');
 } else {
     alert('Zero é neutro.')
-}
+} */
+
+
+//let chute = prompt('Escolha um número entre 1 e 10');
+//console.log('Valor do chute:', chute);
+//alert(chute);
+
+/* alert('Boas vindas ao jogo do número secreto!');
+let chute = prompt('Escolha um número entre 1 e 10');
+console.log('Valor do chute:', chute);
+
+let numeroSecreto = 4;
+console.log(numeroSecreto)
+console.log('Resultado da comaparação:', chute == numeroSecreto);
+
+if ( chute == numeroSecreto ) {
+    alert('Acertou!');
+} else {
+    console.log('Valor do número secreto:', numeroSecreto);
+    alert('O múmero secreto era ' + numeroSecreto);
+} */
 
 
