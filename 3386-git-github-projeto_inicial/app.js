1056bc4446804b3da5b6fecedeb6a07721749ebc
gilsondultra <gdultra@uneb.br>
2025-02-12 22:36:32 -0300

    conclusão do projeto

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -9,10 +9,6 @@ if (numeroInput > 0) {
 } */
 
 
-//let chute = prompt('Escolha um número entre 1 e 10');
-//console.log('Valor do chute:', chute);
-//alert(chute);
-
 /* alert('Boas vindas ao jogo do número secreto!');
 let chute = prompt('Escolha um número entre 1 e 10');
 console.log('Valor do chute:', chute);
@@ -27,5 +23,21 @@ if ( chute == numeroSecreto ) {
     console.log('Valor do número secreto:', numeroSecreto);
     alert('O múmero secreto era ' + numeroSecreto);
 } */
+
+alert('Boas vindas ao  Jogo do Número Secreto');
+let chute = prompt('Escolha um número de 1 a 10');
+console.log('Valor do chute:', chute);
+
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+console.log('Resultado da comparação:', chute == numeroSecreto);
+
+if( chute == numeroSecreto ) {
+    alert('Isso aí! Você acertou!!');
+} else {
+    console.log('Valor do número secreto:', numeroSecreto);
+    alert('Você errou! O númro secreto era:' + numeroSecreto);
+}
+
 
 
